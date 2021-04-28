9c7d346f379252175da6d6c4261447764293db92
emagdaeh <adam.whitmanpt@gmail.com>
2021-04-28 10:37:48 -0700

    Understanding public and private and getters and setters in typescript

--- ts-hello/thirdMain.ts
+++ ts-hello/thirdMain.ts
@@ -1,17 +1,25 @@
 class Point {
-  x: number;
-  y: number;
-
-  draw() {
-    console.log('X: ' + this.x + ', Y: ' + this.y);
+  constructor(private _x?: number, private _y?: number) {
   }
 
-  getDistance(another: Point) {
-    // ...
+  draw() {
+    console.log('X: ' + this._x + ', Y: ' + this._y);
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(value) {
+    if (value < 0) {
+      throw new Error('Value cannot be less than 0.');
+    }
+
+    this._x = value;
   }
 }
 
-let point = new Point();
-point.x = 1;
-point.y = 2;
+let point = new Point(1, 2);
+let x = point.x;
+point.x = 10;
 point.draw();
